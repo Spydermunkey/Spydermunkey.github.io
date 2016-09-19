f929fa072c762f863157d29d9400bdbe24ee76e1
BuildTools <unconfigured@null.spigotmc.org>
2016-09-19 04:56:05 -0700

    Test

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Project Proletariat</title>
+	</head>
+
+	<body>
+		<header>
+			<link rel="import" href="navbar.php">
+		</header>
+	</body>
+
+	<footer>
+	</footer>
+</html>
